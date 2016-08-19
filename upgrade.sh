b18eac1778bffd665b7f346545842cd36a7c3864
Nate Peterson <ntpeters@mtu.edu>
2016-08-19 03:03:47 -0400

    Add info notifications

--- upgrade.sh
+++ upgrade.sh
@@ -2,14 +2,27 @@
 
 CLEANUP=$1
 PACKAGES_TO_UPGRADE="${@:2}"
+PACKAGE_COUNT=$(( $# - 1 ))
 BREW=$(which brew)
 TERMINAL_NOTIFIER=$(which terminal-notifier)
 BEER_ICON=$HOME/.homebrew-notifier/beer-icon.png
 BREW_UPDATE_LOG=/tmp/homebrew-notifier-update.log
 
+$TERMINAL_NOTIFIER -sender com.apple.Terminal \
+    -appIcon $BEER_ICON \
+    -title "Homebrew Updating..." \
+    -subtitle "Update in progress" \
+    -message "Updating $PACKAGE_COUNT formulae..." \
+
 $BREW upgrade "$PACKAGES_TO_UPDATE" > $BREW_UPDATE_LOG
 
 if $CLEANUP; then
+    $TERMINAL_NOTIFIER -sender com.apple.Terminal \
+        -appIcon $BEER_ICON \
+        -title "Homebrew Cleaning..." \
+        -subtitle "Cleanup in progress" \
+        -message "Removing old versions, downloads, and caches." \
+
     $BREW cleanup
 fi
 
