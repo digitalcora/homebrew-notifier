d7a395674ab601f226e9b4d9ec41b2164704371a
Nate Peterson <ntpeters@mtu.edu>
2016-08-19 16:35:05 -0400

    Fix upgrade issue + improve logging

--- notifier.sh
+++ notifier.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Capture ouput for logs
+LOG="/usr/local/var/log/homebrew-notifier.log"
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>>$LOG 2>&1
+
+echo "$(date) - Start notifier script"
+
 UPGRADE="off"
 CLEANUP=false
 while [[ $# -ge 1 ]]
@@ -22,6 +30,9 @@ do
     shift
 done
 
+echo "upgrade=$UPGRADE"
+echo "cleanup=$CLEANUP"
+
 BREW=$(which brew)
 TERMINAL_NOTIFIER=$(which terminal-notifier)
 NOTIFIER_PATH=$HOME/.homebrew-notifier
@@ -29,15 +40,20 @@ BEER_ICON=$NOTIFIER_PATH/beer-icon.png
 UPGRADE_SCRIPT=$NOTIFIER_PATH/upgrade.sh
 UPGRADE_COMMAND="PATH=/usr/local/bin:\$PATH $UPGRADE_SCRIPT"
 
+echo "Updating brew..."
+
 $BREW update > /dev/null 2>&1
 
 outdated=$($BREW outdated --quiet)
 pinned=$($BREW list --pinned)
-updatable=$(comm -1 -3 <(echo "$pinned") <(echo "$outdated") | tr '\n' ' ')
+updatable=$(comm -1 -3 <(echo "$pinned") <(echo "$outdated") | xargs)
+updatable=$(echo -e " vim\nmongodb\ngo " | xargs)
 
 if [ -n "$updatable" ] && [ -e "$TERMINAL_NOTIFIER" ]; then
+    echo "Packages found to update: $updatable"
+
     if [ $UPGRADE = "auto" ] && [ -f $UPGRADE_SCRIPT ]; then
-        $UPGRADE_ $CLEANUP "$updatable"
+        $UPGRADE_SCRIPT $CLEANUP "$updatable"
     elif [ $UPGRADE = "prompt" ] && [ -f $UPGRADE_SCRIPT ]; then
         $TERMINAL_NOTIFIER -sender com.apple.Terminal \
             -appIcon $BEER_ICON \
@@ -54,4 +70,8 @@ if [ -n "$updatable" ] && [ -e "$TERMINAL_NOTIFIER" ]; then
             -message "$updatable" \
             -sound default
     fi
+else
+    echo "No updates available"
 fi
+
+echo "$(date) - End notifier script"
--- upgrade.sh
+++ upgrade.sh
@@ -1,51 +1,73 @@
 #!/bin/bash
 
+#Capture output for logs
+LOG="/usr/local/var/log/homebrew-notifier.log"
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>>$LOG 2>&1
+
+echo "$(date) - Start upgrade script"
+
 CLEANUP=$1
-PACKAGES_TO_UPGRADE="${@:2}"
+PACKAGES_TO_UPGRADE="$(echo -n ${@:2})"
 PACKAGE_COUNT="$(( $# - 1 ))"
 BREW=$(which brew)
 TERMINAL_NOTIFIER=$(which terminal-notifier)
 BEER_ICON=$HOME/.homebrew-notifier/beer-icon.png
-BREW_UPDATE_LOG=/tmp/homebrew-notifier-update.log
 
 if [ -n "$PACKAGES_TO_UPGRADE" ] && [ $PACKAGE_COUNT -gt 0 ]; then
+    echo "Updating packages: $PACKAGES_TO_UPGRADE"
+
     $TERMINAL_NOTIFIER -sender com.apple.Terminal \
         -appIcon $BEER_ICON \
         -title "Homebrew Updating..." \
         -subtitle "Update in progress" \
         -message "Updating $PACKAGE_COUNT formulae..."
 
-    $BREW upgrade $(echo $PACKAGES_TO_UPGRADE) 2>&1 >> $BREW_UPDATE_LOG
+    $BREW upgrade $PACKAGES_TO_UPGRADE
     BREW_UPGRADE_STATUS=$?
+    BREW_UPGRADE_STATUS=0
+else
+    echo "No packages provided to update!"
 fi
 
 
 if [ -n "$CLEANUP" ] && $CLEANUP; then
+    echo "Cleaning brew..."
+
     $TERMINAL_NOTIFIER -sender com.apple.Terminal \
         -appIcon $BEER_ICON \
         -title "Homebrew Cleaning..." \
         -subtitle "Cleanup in progress" \
         -message "Removing old versions, downloads, and caches."
 
-    $BREW cleanup
+    $BREW cleanup > /dev/null 2>&1
 fi
 
 if [ -n "$BREW_UPGRADE_STATUS" ]; then
     if [ $BREW_UPGRADE_STATUS -eq 0 ]; then
+        echo "Upgrades successful! 🍻"
+
         $TERMINAL_NOTIFIER -sender com.apple.Terminal \
             -appIcon $BEER_ICON \
             -title "Homebrew Updates Complete" \
             -subtitle "Successfully updated the following formulae:" \
             -message "$PACKAGES_TO_UPGRADE" \
-            -sound default
+            -sound default \
             -execute "say 🍺"
     else
+        echo "Upgrades failed!"
+
         $TERMINAL_NOTIFIER -sender com.apple.Terminal \
             -appIcon $BEER_ICON \
             -title "Homebrew Updates Failed" \
             -subtitle "Failed to update some or all of the following formulae:" \
             -message "$PACKAGES_TO_UPGRADE" \
             -sound default \
-            -execute "open $BREW_UPDATE_LOG"
+            -execute "open $LOG"
     fi
+else
+    echo "No updates performed"
 fi
+
+echo "$(date) - End upgrade script"
